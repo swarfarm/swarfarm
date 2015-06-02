8b5341dd5b203e79bbdfcf6c35aab682b1d6e30f
Peter <peter@porksmash.com>
2015-06-01 23:36:00 -0700

    Giving up on random feedback apps. Fixed awakening material count class on bestiary page.

--- swarfarm/urls.py
+++ swarfarm/urls.py
@@ -4,5 +4,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('herders.urls', namespace="herders")),
-    url(r'^feedback/', include('django_basic_feedback.urls')),
 ]

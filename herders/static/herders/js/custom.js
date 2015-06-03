15d8f8c1167ffb89d6c412daa992375a73f84ec1
Peter <peter@porksmash.com>
2015-06-03 00:06:21 -0700

    Autofocus on modal show

--- herders/static/herders/js/custom.js
+++ herders/static/herders/js/custom.js
@@ -1,3 +1,12 @@
+//Initialize all bootstrap tooltips
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+//Modal management scripts
+var addMonsterModal = $('#addMonsterModal');
+
+addMonsterModal.on('shown.bs.modal', function () {
+    $('#id_monster-autocomplete').focus()
 });

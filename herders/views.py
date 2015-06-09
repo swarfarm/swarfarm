b5c42b8caa76b6de31325634f73187e6c99d97c2
Peter <peter@porksmash.com>
2015-06-08 23:56:46 -0700

    Added data collection in view for monster fusion progress.

--- herders/views.py
+++ herders/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404, get_list_or_40
 from django.core.urlresolvers import reverse
 from django.core.exceptions import PermissionDenied
 from django.db import IntegrityError
+from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -401,7 +402,377 @@ def fusion(request, profile_name):
         'view': 'fusion',
     }
 
-    return render(request, 'herders/unimplemented.html', context)
+    # Phoenix (Water) - 5*, requires Arang, Jojo, Susano, Mina
+    phoenix_progress = {
+        'instance': Monster.objects.get(pk=646),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=647) | Q(monster__pk=646)
+        ).count() > 0,
+        'stars': 5,
+        'cost': 500000,
+        'ingredients': {
+            'arang': {
+                'instance': Monster.objects.get(pk=534),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=534) | Q(monster__pk=533),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'jojo': {
+                'instance': Monster.objects.get(pk=236),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=236) | Q(monster__pk=235),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'susano': {
+                'instance': Monster.objects.get(pk=603),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=603) | Q(monster__pk=602),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'mina': {
+                'instance': Monster.objects.get(pk=336),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=336) | Q(monster__pk=335),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['phoenix'] = phoenix_progress
+
+    # Valkyrja (Wind) - 5*, requires Baretta, Mikene, Arang, Shakan
+    valkyrja_progress = {
+        'instance': Monster.objects.get(pk=584),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=585) | Q(monster__pk=584)
+        ).count() > 0,
+        'stars': 5,
+        'cost': 500000,
+        'ingredients': {
+            'baretta': {
+                'instance': Monster.objects.get(pk=260),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=260) | Q(monster__pk=259),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'mikene': {
+                'instance': Monster.objects.get(pk=623),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=623) | Q(monster__pk=622),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'arang': {
+                'instance': Monster.objects.get(pk=534),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=534) | Q(monster__pk=533),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'shakan': {
+                'instance': Monster.objects.get(pk=398),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=398) | Q(monster__pk=397),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['valkyrja'] = valkyrja_progress
+
+    # Ifrit (Dark) - 5*, requires Argen, Akia, Mikene, Kumae
+    ifrit_progress = {
+        'instance': Monster.objects.get(pk=773),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=774) | Q(monster__pk=773)
+        ).count() > 0,
+        'stars': 5,
+        'cost': 500000,
+        'ingredients': {
+            'argen': {
+                'instance': Monster.objects.get(pk=557),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=557) | Q(monster__pk=556),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'akia': {
+                'instance': Monster.objects.get(pk=258),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=258) | Q(monster__pk=257),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'mikene': {
+                'instance': Monster.objects.get(pk=623),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=623) | Q(monster__pk=622),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'kumae': {
+                'instance': Monster.objects.get(pk=808),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=808) | Q(monster__pk=519),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['ifrit'] = ifrit_progress
+
+    # Nine-tailed Fox (Wind) - 4*, requires Prilea, Hina, Kahli, Konamiya
+    nine_tailed_fox_progress = {
+        'instance': Monster.objects.get(pk=533),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=534) | Q(monster__pk=533)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'prilea': {
+                'instance': Monster.objects.get(pk=372),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=372) | Q(monster__pk=371),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'hina': {
+                'instance': Monster.objects.get(pk=350),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=350) | Q(monster__pk=556),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'kahli': {
+                'instance': Monster.objects.get(pk=194),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=194) | Q(monster__pk=193),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'konamiya': {
+                'instance': Monster.objects.get(pk=56),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=56) | Q(monster__pk=55),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['nine_tailed_fox'] = nine_tailed_fox_progress
+
+    # Joker (Fire) - 4*, requires Garoche, Cassandra, Kuhn, Chichi
+    joker_progress = {
+        'instance': Monster.objects.get(pk=235),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=236) | Q(monster__pk=235)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'garoche': {
+                'instance': Monster.objects.get(pk=226),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=226) | Q(monster__pk=225),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'cassandra': {
+                'instance': Monster.objects.get(pk=206),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=206) | Q(monster__pk=205),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'kuhn': {
+                'instance': Monster.objects.get(pk=314),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=314) | Q(monster__pk=313),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'chichi': {
+                'instance': Monster.objects.get(pk=102),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=102) | Q(monster__pk=101),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['joker'] = joker_progress
+
+    # Ninja (Water) - 4*, requires Icaru, Kahn, Dagorr, Tantra
+    ninja_progress = {
+        'instance': Monster.objects.get(pk=602),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=603) | Q(monster__pk=602)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'icaru': {
+                'instance': Monster.objects.get(pk=328),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=328) | Q(monster__pk=327),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'kahn': {
+                'instance': Monster.objects.get(pk=316),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=316) | Q(monster__pk=315),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'dagorr': {
+                'instance': Monster.objects.get(pk=354),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=354) | Q(monster__pk=353),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'tantra': {
+                'instance': Monster.objects.get(pk=35),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=35) | Q(monster__pk=34),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['ninja'] = ninja_progress
+
+    # Sylph (Fire) - 4*, requires Fao, Mei, Sharron, Lukan
+    sylph_progress = {
+        'instance': Monster.objects.get(pk=259),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=260) | Q(monster__pk=259)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'fao': {
+                'instance': Monster.objects.get(pk=218),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=218) | Q(monster__pk=217),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'mei': {
+                'instance': Monster.objects.get(pk=208),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=208) | Q(monster__pk=207),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'sharron': {
+                'instance': Monster.objects.get(pk=334),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=334) | Q(monster__pk=333),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'lukan': {
+                'instance': Monster.objects.get(pk=108),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=108) | Q(monster__pk=107),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['sylph'] = sylph_progress
+
+    # Undine (Water) - 4*, requires Gruda, Hemos, Anduril, Cogma
+    undine_progress = {
+        'instance': Monster.objects.get(pk=259),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=260) | Q(monster__pk=259)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'gruda': {
+                'instance': Monster.objects.get(pk=300),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=300) | Q(monster__pk=299),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'hemos': {
+                'instance': Monster.objects.get(pk=318),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=318) | Q(monster__pk=317),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'anduril': {
+                'instance': Monster.objects.get(pk=378),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=378) | Q(monster__pk=377),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'cogma': {
+                'instance': Monster.objects.get(pk=29),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=29) | Q(monster__pk=28),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['undine'] = undine_progress
+
+    # Vampire (Wind) - 4*, requires Eintau, Velfinodon, Iron, Kacey
+    vampire_progress = {
+        'instance': Monster.objects.get(pk=556),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=556) | Q(monster__pk=557)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'eintau': {
+                'instance': Monster.objects.get(pk=390),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=390) | Q(monster__pk=389),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'velfinodon': {
+                'instance': Monster.objects.get(pk=384),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=384) | Q(monster__pk=383),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'iron': {
+                'instance': Monster.objects.get(pk=202),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=202) | Q(monster__pk=201),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'kacey': {
+                'instance': Monster.objects.get(pk=66),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=66) | Q(monster__pk=65),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['vampire'] = vampire_progress
+
+    # Succubus (Fire) - 4*, requires Nangrim, Krakdon, Ramira, Seal
+    succubus_progress = {
+        'instance': Monster.objects.get(pk=257),
+        'acquired': MonsterInstance.objects.filter(
+            Q(owner=request.user.summoner), Q(monster__pk=258) | Q(monster__pk=257)
+        ).count() > 0,
+        'stars': 4,
+        'cost': 100000,
+        'ingredients': {
+            'nangrim': {
+                'instance': Monster.objects.get(pk=178),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=178) | Q(monster__pk=170),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'krakdon': {
+                'instance': Monster.objects.get(pk=216),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=216) | Q(monster__pk=215),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'ramira': {
+                'instance': Monster.objects.get(pk=320),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=320) | Q(monster__pk=319),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+            'seal': {
+                'instance': Monster.objects.get(pk=98),
+                'owned': MonsterInstance.objects.filter(
+                    Q(owner=request.user.summoner), Q(monster__pk=98) | Q(monster__pk=97),
+                ).order_by('-stars', '-level', '-monster__is_awakened').first(),
+            },
+        }
+    }
+    context['succubus'] = succubus_progress
+
+    return render(request, 'herders/fusion.html', context)
 
 
 @login_required

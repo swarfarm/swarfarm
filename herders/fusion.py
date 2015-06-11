1bc8fcc60dbece89fc5f6ee7a043b156ecc3a3bb
Peter <peter@porksmash.com>
2015-06-11 00:06:37 -0700

    Mega improved fusion layout.

--- herders/fusion.py
+++ herders/fusion.py
@@ -10,9 +10,11 @@ def fusion_progress(summoner, product_monster_id, stars, cost, ingredient_monste
     product_instance = Monster.objects.get(pk=product_monster_id)
 
     # Indicate if already in summoner's collection
-    acquired = MonsterInstance.objects.filter(
+    fusion_complete = MonsterInstance.objects.filter(
         Q(owner=summoner), Q(monster__pk=product_instance.pk) | Q(monster__pk=product_instance.awakens_to().pk)
     ).count() > 0,
+
+    level = 10 + stars * 5
 
     # Collect all the ingredients
     ingredients = []
@@ -20,26 +22,40 @@ def fusion_progress(summoner, product_monster_id, stars, cost, ingredient_monste
         ingredient_instance = Monster.objects.get(pk=ingredient_id)
 
         owned_ingredients = MonsterInstance.objects.filter(
-            Q(owner=summoner) & (
-                Q(monster__pk=ingredient_instance.pk) | Q(monster__pk=ingredient_instance.awakens_from.pk)),
+            Q(owner=summoner),
+            Q(monster__pk=ingredient_instance.pk) | Q(monster__pk=ingredient_instance.awakens_from.pk),
         ).order_by('-stars', '-level', '-monster__is_awakened')
 
-        complete = MonsterInstance.objects.filter(
-            owner=summoner, monster__pk=ingredient_instance.pk, stars=stars, level=10 + stars * 5
-        ).count() > 0
+        # Determine if each individual requirement is met using highest evolved/leveled monster
+        if len(owned_ingredients) > 0:
+            acquired = True
+            evolved = owned_ingredients[0].stars >= stars
+            leveled = owned_ingredients[0].level >= level
+            awakened = owned_ingredients[0].monster.pk == ingredient_instance.pk
+            complete = acquired & evolved & leveled & awakened
+        else:
+            acquired = False
+            evolved = False
+            leveled = False
+            awakened = False
+            complete = False
 
         ingredient = {
             'instance': ingredient_instance,
             'owned': owned_ingredients,
             'complete': complete,
+            'acquired': acquired,
+            'evolved': evolved,
+            'leveled': leveled,
+            'awakened': awakened,
         }
         ingredients.append(ingredient)
 
     return {
         'instance': product_instance,
-        'acquired': acquired,
+        'acquired': fusion_complete,
         'stars': stars,
-        'level': 10 + stars * 5,
+        'level': level,
         'cost': cost,
         'ingredients': ingredients,
         'ready': _all_ingredients_satisfied(ingredients)

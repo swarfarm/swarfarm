6a680668941583017ff73f3ee4521f3ca19cc455
Peter <peter@porksmash.com>
2015-06-12 16:14:23 -0700

    Customized autocomplete light monster results

--- herders/autocomplete_light_registry.py
+++ herders/autocomplete_light_registry.py
@@ -1,14 +1,21 @@
 import autocomplete_light
 from models import Monster
 
+
+class MonsterAutocomplete(autocomplete_light.AutocompleteModelTemplate):
+    choice_template = 'monster_choice_autocomplete.html'
+
+
 autocomplete_light.register(
     Monster,
+    MonsterAutocomplete,
+    choices=Monster.objects.all().order_by('is_awakened', 'element'),
     search_fields=['name', 'awakens_from__name'],
     attrs={
         'placeholder': 'Start typing monster name',
         'data-autocomplete-minimum-characters': 2,
     },
     widget_attrs={
-        'data-widget-maximum-values': 10,
-    },
+        'data-widget-maximum-values': 6,
+    }
 )

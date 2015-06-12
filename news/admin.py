317508d98300f5c6e2d459704cbc2701f9b513fa
Peter <peter@porksmash.com>
2015-06-12 11:59:48 -0700

    Added more fields to news admin interface

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Article
 
-# Register your models here.
-admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_until', 'is_active')

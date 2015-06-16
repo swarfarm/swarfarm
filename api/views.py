ebfc587792ea1503817918c6732ffd895a02f014
Peter <peter@porksmash.com>
2015-06-16 11:17:22 -0700

    Update filter button state on page load from saved filters

--- api/views.py
+++ api/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.core import serializers
 from django.http import HttpResponse
 
--- herders/static/herders/js/custom.js
+++ herders/static/herders/js/custom.js
@@ -21,7 +21,16 @@ $('#id_monster-autocomplete').bind('selectChoice',
     });
 
 $(document).ready(function() {
-    $('#monster_table').tablesorter({
+    var monster_table = $('#monster_table');
+
+    var filter_buttons = $('button.filter');
+
+    var element_filter_col = 4;
+    var archetype_filter_col = 5;
+    var priority_filter_col = 6;
+
+
+    monster_table.tablesorter({
         widgets: ['filter', 'saveSort'],
         ignoreCase: true,
         widgetOptions: {
@@ -34,7 +43,72 @@ $(document).ready(function() {
         }
     });
 
-    $('button.filter').click(function() {
+    //Get list of current filters
+    var current_filters = $.tablesorter.getFilters(monster_table);
+
+    if (current_filters) {
+        //Element buttons
+        if (current_filters[element_filter_col].includes("fire")){
+            $(filter_buttons.get(0)).toggleClass('active', true);
+        }
+
+        if (current_filters[element_filter_col].includes("water")){
+            $(filter_buttons.get(1)).toggleClass('active', true);
+        }
+
+        if (current_filters[element_filter_col].includes("wind")){
+            $(filter_buttons.get(2)).toggleClass('active', true);
+        }
+
+        if (current_filters[element_filter_col].includes("dark")){
+            $(filter_buttons.get(3)).toggleClass('active', true);
+        }
+
+        if (current_filters[element_filter_col].includes("light")){
+            $(filter_buttons.get(4)).toggleClass('active', true);
+        }
+
+        //Archetype buttons
+        if (current_filters[archetype_filter_col].includes("Attack")){
+            $(filter_buttons.get(5)).toggleClass('active', true);
+        }
+
+        if (current_filters[archetype_filter_col].includes("Defense")){
+            $(filter_buttons.get(6)).toggleClass('active', true);
+        }
+
+        if (current_filters[archetype_filter_col].includes("Hp")){
+            $(filter_buttons.get(7)).toggleClass('active', true);
+        }
+
+        if (current_filters[archetype_filter_col].includes("Support")){
+            $(filter_buttons.get(8)).toggleClass('active', true);
+        }
+
+        if (current_filters[archetype_filter_col].includes("Material")){
+            $(filter_buttons.get(9)).toggleClass('active', true);
+        }
+
+        //Priority buttons
+        if (current_filters[priority_filter_col].includes("High")){
+            $(filter_buttons.get(10)).toggleClass('active', true);
+        }
+
+        if (current_filters[priority_filter_col].includes("Medium")){
+            $(filter_buttons.get(11)).toggleClass('active', true);
+        }
+
+        if (current_filters[priority_filter_col].includes("Low")){
+            $(filter_buttons.get(12)).toggleClass('active', true);
+        }
+
+        if (current_filters[priority_filter_col].includes("Done")){
+            $(filter_buttons.get(13)).toggleClass('active', true);
+        }
+    }
+
+
+    filter_buttons.click(function() {
         $( this ).toggleClass('active');
 
         var filters = $('#monster_table').find('input.tablesorter-filter'),

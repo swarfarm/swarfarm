82bcd2dfdbe31e15e527330238c40e34684897a9
Peter <peter@porksmash.com>
2015-06-17 15:48:43 -0700

    Created a script to update the database

--- herders/urls.py
+++ herders/urls.py
@@ -35,4 +35,6 @@ urlpatterns = [
         url(r'^(?P<monster_element>[a-zA-Z]+)/$', views.bestiary, name='bestiary_element'),
         url(r'^(?P<monster_id>[0-9]+)/$', views.bestiary_detail, name='bestiary_detail'),
     ])),
+
+    url(r'^fixdb/$', views.fixdb),
 ]

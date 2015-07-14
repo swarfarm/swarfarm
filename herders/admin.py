86a46a9295916c129f1213ba812f51a232a05fe0
peter <peter@porksmash.com>
2015-07-13 22:16:43 -0700

    Added elemental leader field to monster skills. More skill icons too.

--- herders/admin.py
+++ herders/admin.py
@@ -74,7 +74,7 @@ admin.site.register(Monster, MonsterAdmin)
 
 
 class MonsterSkillAdmin(admin.ModelAdmin):
-    list_display = ('image_url', 'name', 'icon_filename', 'description', 'slot', 'passive', 'general_leader', 'dungeon_leader', 'arena_leader', 'guild_leader')
+    list_display = ('image_url', 'name', 'icon_filename', 'description', 'slot', 'passive', 'general_leader', 'element_leader', 'dungeon_leader', 'arena_leader', 'guild_leader')
     filter_vertical = ('skill_effect',)
     list_filter = ('general_leader', 'dungeon_leader', 'arena_leader', 'guild_leader')
     search_fields = ['name', 'icon_filename']

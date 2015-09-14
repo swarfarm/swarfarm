fbab1abe7737062faa5f90c4e9328c4b0a0d79f5
Peter <peter@porksmash.com>
2015-09-14 10:22:24 -0700

    Removed /list from example URL

--- herders/forms.py
+++ herders/forms.py
@@ -104,7 +104,7 @@ class RegisterUserForm(forms.Form):
     username = forms.CharField(
         label='Username',
         required=True,
-        help_text='Used to link to your profile to others: http://swarfarm.com/profile/&lt;username&gt;/view',
+        help_text='Used to link to your profile to others: http://swarfarm.com/profile/&lt;username&gt;/',
         validators=[
             RegexValidator(
                 regex='^[a-zA-Z0-9_]+$',

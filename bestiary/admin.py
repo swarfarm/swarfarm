02b177afc246bda62d7db8eabe15ca86d5ade154
peter <peter@porksmash.com>
2016-02-22 19:56:35 -0800

    Added a note field to skill effect details, changed Chance to allow nulls.

--- bestiary/admin.py
+++ bestiary/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import TextInput
 from .models import *
 
 
@@ -154,6 +156,9 @@ class MonsterSkillScalesWithInline(admin.TabularInline):
 class MonsterSkillEffectDetailInline(admin.TabularInline):
     model = EffectDetail
     extra = 5
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
 
 
 @admin.register(Skill)

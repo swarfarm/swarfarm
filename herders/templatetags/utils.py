de0473249d6cf3e4648a320bba21184b81c5b665
porksmash <peter@porksmash.com>
2016-06-10 16:14:18 -0700

    Added humanize filter that works on thousands"

--- herders/templatetags/utils.py
+++ herders/templatetags/utils.py
@@ -36,3 +36,20 @@ def remove_extension(string):
 def key(d, key_name):
     return d[key_name]
 key = register.filter('key', key)
+
+
+@register.filter
+def humanize_number(value):
+    powers = [10 ** x for x in (3, 6, 9, 12)]
+    human_powers = ('K', 'M', 'B', 'T')
+    try:
+        index, hp = next((i, p) for i, p in enumerate(human_powers)
+                         if 10 ** 3 > value / powers[i] > 0)
+        return_value = "%.{fraction_point}f".format(
+            fraction_point=1) % (float(value) / powers[index])
+        return_value = return_value \
+            if float(return_value) != int(float(return_value)) \
+            else str(int(float(return_value)))
+        return "%s%s" % (return_value, hp)
+    except (IndexError, StopIteration):
+        return value

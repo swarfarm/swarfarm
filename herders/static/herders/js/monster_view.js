164d57caec324328f1ba72b06ccfc9b8ec0d9633
porksmash <peter@porksmash.com>
2016-10-04 15:01:43 -0700

    Fixed grabbing the right slot from the filter form when creating a new rune on a monster

--- herders/static/herders/js/monster_view.js
+++ herders/static/herders/js/monster_view.js
@@ -290,7 +290,7 @@ $('body')
     .on('click', '.monster-delete', function() { DeleteMonster($(this).data('instance-id')) })
     .on('click', '.monster-awaken', function() { AwakenMonster($(this).data('instance-id')) })
     .on('click', '.monster-power-up', function() { PowerUpMonster($(this).data('instance-id')) })
-    .on('click', '#addNewRune', function() { CreateNewRune($("#id_slot").val()) })
+    .on('click', '#addNewRune', function() { CreateNewRune($("#id_assign-slot_0").val()) })
     .on('submit', '#AssignRuneForm', function() {
         var $form = $(this);
         $.ajax({

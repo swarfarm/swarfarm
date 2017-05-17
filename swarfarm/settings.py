330139569478096f05ecc69c2f691b3726b91acb
porksmash <peter@porksmash.com>
2017-05-17 13:51:25 -0700

    Updated INTERNAL_IPS so django-debug-toolbar works correctly when running locally instead of on the Vagrant VM

--- swarfarm/settings.py
+++ swarfarm/settings.py
@@ -29,7 +29,7 @@ WSGI_APPLICATION = 'swarfarm.wsgi.application'
 
 # Security settings
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
-INTERNAL_IPS = ['127.0.0.1']
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 
 if DEBUG:
     ALLOWED_HOSTS += ['10.0.2.2', '10.243.243.10']

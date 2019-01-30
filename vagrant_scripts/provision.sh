9b8194ba32397c1c82cbf18bc6493450ea9ea2f6
Peter Andersen <peter@porksmash.com>
2019-01-29 21:09:02 -0800

    Updated Vagrant configuration to ubuntu 18.04

--- vagrant_scripts/provision.sh
+++ vagrant_scripts/provision.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # Add postgresql repo
-echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Install packages
 echo "Installing software packages..."
-sudo apt-get -qq update
-sudo apt-get -qq upgrade -y
-sudo apt-get -qq install -y nginx postgresql-9.4 postgresql-contrib-9.4 libpq-dev pgbouncer rabbitmq-server redis-server git make build-essential libjpeg-dev libffi-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev
+sudo apt -qq update
+sudo apt -qq upgrade -y
+sudo apt -qq install -y nginx postgresql-11 libpq-dev rabbitmq-server redis-server git make build-essential libjpeg-dev libffi-dev libssl-dev zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev
 
 # Postgresql setup
-# Allow listening on all interfaces
+# Allow listening on all interfaces to connect directly from host PC
 echo "Setting up postgresql database and roles..."
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "/etc/postgresql/9.4/main/postgresql.conf"
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "/etc/postgresql/11/main/postgresql.conf"
 # Append to pg_hba.conf to add password auth:
-echo "host    all             all             all                     md5" | sudo tee --append /etc/postgresql/9.4/main/pg_hba.conf > /dev/null
+echo "host    all             all             all                     md5" | sudo tee --append /etc/postgresql/11/main/pg_hba.conf > /dev/null
 
 sudo service postgresql restart
 
@@ -24,17 +24,11 @@ sudo -u postgres psql --command="CREATE DATABASE swarfarm_dev;"
 sudo -u postgres psql --command="CREATE USER swarfarmer_dev WITH PASSWORD 'intentionallyweak';"
 sudo -u postgres psql --command="GRANT ALL PRIVILEGES ON DATABASE swarfarm_dev TO swarfarmer_dev;"
 
-sudo mv ~/pgbouncer.ini /etc/pgbouncer/pgbouncer.ini
-sudo mv ~/pgbouncer_userlist.txt /etc/pgbouncer/userlist.txt
-sudo mv ~/pgbouncer_start /etc/default/pgbouncer
-sudo service pgbouncer restart
-
 # Set up nginx
 sudo mv ~/nginx_config /etc/nginx/sites-available/swarfarm
 sudo ln -s /etc/nginx/sites-available/swarfarm /etc/nginx/sites-enabled/swarfarm
-sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx reload
-
 
 # Install pyenv + friends
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
@@ -48,9 +42,9 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
 # Install python + init virtualenv
-pyenv install 3.6.7
-pyenv virtualenv 3.6.7 swarfarm-3.6.7
-pyenv activate swarfarm-3.6.7
+pyenv install 3.6.8
+pyenv virtualenv 3.6.8 swarfarm-3.6.8
+pyenv activate swarfarm-3.6.8
 
 echo "Setting up python environment..."
 pip install -qq -r /vagrant/requirements_dev.txt
@@ -63,10 +57,12 @@ echo "Loading initial bestiary data..."
 python manage.py loaddata bestiary_data.json
 
 # Configure upstart and start python processes
-sudo mv ~/gunicorn_upstart.conf /etc/init/swarfarm.conf
-sudo mv ~/celery_upstart.conf /etc/init/celeryd.conf
-sudo mv ~/celery_beat_upstart.conf /etc/init/celerybeat.conf
+sudo mv ~/swarfarm.socket /etc/systemd/system/swarfarm.socket
+sudo mv ~/swarfarm.service /etc/systemd/system/swarfarm.service
+sudo mv ~/celery.service /etc/systemd/system/celery.service
+sudo mv ~/celery_beat.service /etc/systemd/system/celery_beat.service
 
+sudo systemctl daemon-reload
 sudo service swarfarm start
 sudo service celeryd start
 sudo service celerybeat start

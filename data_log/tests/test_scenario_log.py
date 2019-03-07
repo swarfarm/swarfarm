81229be8e7099a229d1eef4b6144a10303fa9f98
Peter Andersen <peter@porksmash.com>
2019-03-06 22:42:11 -0800

    Corrected folder name for BattleScenarioResult command data

--- data_log/tests/test_scenario_log.py
+++ data_log/tests/test_scenario_log.py
@@ -7,7 +7,7 @@ class ScenarioLogTests(BaseLogTest):
     fixtures = ['test_game_items', 'test_levels', 'test_summon_monsters']
 
     def test_scenario_result(self):
-        self._do_log('ScenarioBattleResult/garen_normal_rune_drop.json')
+        self._do_log('BattleScenarioResult/garen_normal_rune_drop.json')
 
         self.assertEqual(models.DungeonLog.objects.count(), 1)
 
@@ -16,38 +16,38 @@ class ScenarioLogTests(BaseLogTest):
         self.assertIsNotNone(log.clear_time)
 
     def test_level_parsed_correctly(self):
-        self._do_log('ScenarioBattleResult/garen_normal_rune_drop.json')
+        self._do_log('BattleScenarioResult/garen_normal_rune_drop.json')
         log = models.DungeonLog.objects.last()
         self.assertEqual(log.level.dungeon.pk, 1)
         self.assertEqual(log.level.difficulty, Level.DIFFICULTY_NORMAL)
         self.assertEqual(log.level.floor, 1)
 
-        self._do_log('ScenarioBattleResult/kabir_hard_rune_drop.json')
+        self._do_log('BattleScenarioResult/kabir_hard_rune_drop.json')
         log = models.DungeonLog.objects.last()
         self.assertEqual(log.level.dungeon.pk, 3)
         self.assertEqual(log.level.difficulty, Level.DIFFICULTY_HARD)
         self.assertEqual(log.level.floor, 1)
 
-        self._do_log('ScenarioBattleResult/faimon_hell_craft_drop.json')
+        self._do_log('BattleScenarioResult/faimon_hell_craft_drop.json')
         log = models.DungeonLog.objects.last()
         self.assertEqual(log.level.dungeon.pk, 9)
         self.assertEqual(log.level.difficulty, Level.DIFFICULTY_HELL)
         self.assertEqual(log.level.floor, 4)
 
     def test_scenario_rune_drop(self):
-        self._do_log('ScenarioBattleResult/garen_normal_rune_drop.json')
+        self._do_log('BattleScenarioResult/garen_normal_rune_drop.json')
 
         log = models.DungeonLog.objects.first()
         self.assertEqual(log.dungeonrunedrop_set.count(), 1)
 
     def test_scenario_monster_drop(self):
-        self._do_log('ScenarioBattleResult/faimon_hell_monster_drop.json')
+        self._do_log('BattleScenarioResult/faimon_hell_monster_drop.json')
 
         log = models.DungeonLog.objects.first()
         self.assertEqual(log.dungeonmonsterdrop_set.count(), 1)
 
     def test_scenario_item_drop(self):
-        self._do_log('ScenarioBattleResult/faimon_hell_craft_drop.json')
+        self._do_log('BattleScenarioResult/faimon_hell_craft_drop.json')
 
         log = models.DungeonLog.objects.first()
         # Expect Mana, Energy, and Craft Item

e289b6cd9a66b86232109731871347ace8b1045d
Peter Andersen <peter@porksmash.com>
2019-05-13 21:09:16 -0700

    Moved some common chart parsing to a function, added coloring for rune based series

--- data_log/templatetags/report_charts.py
+++ data_log/templatetags/report_charts.py
@@ -1,7 +1,7 @@
 from django import template
 from copy import deepcopy
 import json
-
+from bestiary.models import RuneObjectBase
 from data_log.reports import chart_templates
 
 register = template.Library()
@@ -26,9 +26,65 @@ def drop_summary(summary_data):
     return json.dumps(chart_data)
 
 
-def pie(data, *args, **kwargs):
-    chart_data = deepcopy(chart_templates.pie)
+def _common_chart_attributes(chart_template, **kwargs):
+    chart_data = deepcopy(chart_template)
     chart_data['title']['text'] = kwargs.get('title')
+
+    if kwargs.get('legend'):
+        chart_data['legend'] = {
+            'useHTML': True,
+            'align': 'right',
+            'x': 0,
+            'verticalAlign': 'top',
+            'y': 25,
+            'floating': True,
+            'backgroundColor': 'white',
+            'borderColor': '#CCC',
+            'borderWidth': 1,
+            'shadow': False,
+        }
+
+    if kwargs.get('percentage'):
+        chart_data['yAxis']['max'] = 100
+        chart_data['yAxis']['labels'] = {
+            'format': '{value}%'
+        }
+
+    return chart_data
+
+
+_quality_choices = dict(RuneObjectBase.QUALITY_CHOICES)
+QUALITY_COLORS = {
+    _quality_choices[RuneObjectBase.QUALITY_NORMAL]: '#eeeeee',
+    _quality_choices[RuneObjectBase.QUALITY_MAGIC]: '#04d25e',
+    _quality_choices[RuneObjectBase.QUALITY_RARE]: '#3bc9fe',
+    _quality_choices[RuneObjectBase.QUALITY_HERO]: '#d66ef6',
+    _quality_choices[RuneObjectBase.QUALITY_LEGEND]: '#ef9f24',
+}
+
+
+def _color_rune_series(series_data):
+    for series in series_data:
+        if series['name'] in QUALITY_COLORS:
+            series['color'] = QUALITY_COLORS[series['name']]
+
+    return series_data
+
+
+_series_colors = {
+    'rune': _color_rune_series,
+}
+
+
+def color_series(chart_data, color_type):
+    if color_type in _series_colors:
+        chart_data['series'] = _series_colors[color_type](chart_data['series'])
+
+    return chart_data
+
+
+def pie(data, **kwargs):
+    chart_data = _common_chart_attributes(chart_templates.pie, **kwargs)
     chart_data['series'].append({
         'name': '',
         'colorByPoint': True,
@@ -39,42 +95,34 @@ def pie(data, *args, **kwargs):
             } for k, v in data.items()
         ],
     })
+
     return chart_data
 
 
-def bar(data, *args, **kwargs):
-    # Construct series
-    series = {
+def bar(data, **kwargs):
+    chart_data = _common_chart_attributes(chart_templates.column, **kwargs)
+    if kwargs.get('percentage'):
+        total = float(kwargs.get('total', 1)) / 100
+    else:
+        total = 1
 
-    }
-
-    chart_data = deepcopy(chart_templates.column)
-    chart_data['title']['text'] = kwargs.get('title')
-    # chart_data['plotOptions']['column']['stacking'] = 'normal'
+    chart_data['plotOptions']['column']['stacking'] = kwargs.get('stacking')
     chart_data['plotOptions']['column']['groupPadding'] = 0
     chart_data['plotOptions']['column']['pointPadding'] = 0.05
-    chart_data['xAxis']['categories'] = kwargs.get('categories')
+    chart_data['xAxis']['categories'] = kwargs.get('categories', [k for k in data.keys()])
     chart_data['series'] = [{
-        'name': k,
-        'data': v,
-    } for k, v in series.items()]
-    chart_data['legend'] = {
-        'useHTML': True,
-        'align': 'right',
-        'x': 0,
-        'verticalAlign': 'top',
-        'y': 25,
-        'floating': True,
-        'backgroundColor': 'white',
-        'borderColor': '#CCC',
-        'borderWidth': 1,
-        'shadow': False,
-    }
+        'name': 'Main Stats',
+        'data': [float(v) / total for v in data.values()],
+    }]
+
+    chart_data = color_series(chart_data, kwargs.get('colors'))
 
     return chart_data
 
 
-def histogram(data, *args, **kwargs):
+def histogram(data, **kwargs):
+    chart_data = _common_chart_attributes(chart_templates.column, **kwargs)
+
     categories = [
         i['bin'] for i in data
     ]
@@ -91,8 +139,6 @@ def histogram(data, *args, **kwargs):
 
             series[k] += [v]
 
-    chart_data = deepcopy(chart_templates.column)
-    chart_data['title']['text'] = kwargs.get('title')
     chart_data['plotOptions']['column']['stacking'] = 'normal'
     chart_data['plotOptions']['column']['groupPadding'] = 0
     chart_data['plotOptions']['column']['pointPadding'] = 0.05
@@ -101,18 +147,8 @@ def histogram(data, *args, **kwargs):
         'name': k,
         'data': v,
     } for k, v in series.items()]
-    chart_data['legend'] = {
-        'useHTML': True,
-        'align': 'right',
-        'x': 0,
-        'verticalAlign': 'top',
-        'y': 25,
-        'floating': True,
-        'backgroundColor': 'white',
-        'borderColor': '#CCC',
-        'borderWidth': 1,
-        'shadow': False,
-    }
+
+    chart_data = color_series(chart_data, kwargs.get('colors'))
 
     return chart_data
 
@@ -127,5 +163,5 @@ chart_types = {
 @register.simple_tag
 def chart(data, *args, **kwargs):
     chart_type = kwargs.get('type', data['type'])
-    chart_data = chart_types[chart_type](data['data'], *args, **kwargs)
+    chart_data = chart_types[chart_type](**data, **kwargs)
     return json.dumps(chart_data)

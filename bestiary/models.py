e09934451c1e7a2a5c8e594e390b24233ecdce74
Peter Andersen <peter@porksmash.com>
2019-09-04 19:44:14 -0700

    Added slug field to gameitem

--- bestiary/models.py
+++ bestiary/models.py
@@ -2108,6 +2108,7 @@ class GameItem(models.Model):
     name = models.CharField(max_length=200)
     icon = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    slug = models.CharField(max_length=200)
     sell_value = models.IntegerField(blank=True, null=True)
 
     class Meta:
@@ -2122,6 +2123,10 @@ class GameItem(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def image_tag(self):
         if self.icon:

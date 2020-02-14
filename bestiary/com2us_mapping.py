a0b56828107c8eeb55a4d246f0ef160ae2901112
Peter Andersen <peter@porksmash.com>
2020-02-14 12:25:17 -0800

    Cleaned up unused references in com2us_mapping

--- bestiary/com2us_mapping.py
+++ bestiary/com2us_mapping.py
@@ -1,86 +1,6 @@
-from bestiary.models import Monster, LeaderSkill, Rune, RuneCraft
+from bestiary.models import Rune
 
 # TODO: Move these definitions to the models if not already done, delete this file.
-
-element_map = {
-    1: Monster.ELEMENT_WATER,
-    2: Monster.ELEMENT_FIRE,
-    3: Monster.ELEMENT_WIND,
-    4: Monster.ELEMENT_LIGHT,
-    5: Monster.ELEMENT_DARK,
-    6: Monster.ELEMENT_PURE,
-}
-
-archetype_map = {
-    0: Monster.TYPE_NONE,
-    1: Monster.TYPE_ATTACK,
-    2: Monster.TYPE_DEFENSE,
-    3: Monster.TYPE_HP,
-    4: Monster.TYPE_SUPPORT,
-    5: Monster.TYPE_MATERIAL
-}
-
-leader_skill_stat_map = {
-    1: LeaderSkill.ATTRIBUTE_HP,
-    2: LeaderSkill.ATTRIBUTE_ATK,
-    3: LeaderSkill.ATTRIBUTE_DEF,
-    4: LeaderSkill.ATTRIBUTE_SPD,
-    5: LeaderSkill.ATTRIBUTE_CRIT_RATE,
-    6: LeaderSkill.ATTRIBUTE_CRIT_DMG,
-    7: LeaderSkill.ATTRIBUTE_RESIST,
-    8: LeaderSkill.ATTRIBUTE_ACCURACY,
-}
-
-leader_skill_area_map = {
-    0: LeaderSkill.AREA_GENERAL,
-    1: LeaderSkill.AREA_ARENA,
-    2: LeaderSkill.AREA_DUNGEON,
-    5: LeaderSkill.AREA_GUILD,
-}
-
-building_id_map = {
-    'summoners_tower': 1,
-    'summonhenge': 2,
-    'pond_of_mana': 3,
-    'crystal_mine': 4,
-    'gateway': 8,
-    'temple_of_wishes': 10,
-    'magic_shop': 11,
-    'ancient_stones': 12,
-    'arcane_tower': 14,
-    'crystal_titan': 15,
-    'fusion_hexagram': 16,
-    'fuse_center': 17,
-    'power_up_circle': 20,
-    'tranquil_forest': 22,
-    'gusty_cliffs': 23,
-    'deep_forest_ent': 24,
-    'monster_storage': 25,
-    'transmogrification_building': 27,
-}
-
-decoration_id_map = {
-    'guardstone': 4,
-    'mana_fountain': 5,
-    'sky_tribe_totem': 6,
-    'arcane_booster_tower': 7,
-    'crystal_altar': 8,
-    'ancient_sword': 9,
-    'sanctum_of_energy': 10,
-    'mysterious_plant': 11,
-    'fire_sanctuary': 15,
-    'water_sanctuary': 16,
-    'wind_sanctuary': 17,
-    'light_sanctuary': 18,
-    'dark_sanctuary': 19,
-    'fallen_ancient_guardian': 31,
-    'crystal_rock': 34,
-    'fairy_tree': 35,
-    'flag_of_battle': 36,
-    'flag_of_rage': 37,
-    'flag_of_hope': 38,
-    'flag_of_will': 39,
-}
 
 inventory_type_map = {
     'monster': 1,
@@ -205,19 +125,4 @@ rune_quality_map = {
     3: Rune.QUALITY_RARE,
     4: Rune.QUALITY_HERO,
     5: Rune.QUALITY_LEGEND,
-}
-
-craft_type_map = {
-    1: RuneCraft.CRAFT_ENCHANT_GEM,
-    2: RuneCraft.CRAFT_GRINDSTONE,
-    3: RuneCraft.CRAFT_IMMEMORIAL_GEM,
-    4: RuneCraft.CRAFT_IMMEMORIAL_GRINDSTONE,
-}
-
-craft_quality_map = {
-    1: RuneCraft.QUALITY_NORMAL,
-    2: RuneCraft.QUALITY_MAGIC,
-    3: RuneCraft.QUALITY_RARE,
-    4: RuneCraft.QUALITY_HERO,
-    5: RuneCraft.QUALITY_LEGEND,
 }
--- herders/profile_parser.py
+++ herders/profile_parser.py
@@ -1,13 +1,8 @@
-
-import json
-
-import dpkt
 from dateutil.parser import *
 from django.utils.timezone import get_current_timezone
 from jsonschema.exceptions import best_match
 
 from bestiary import com2us_mapping
-from bestiary.com2us_data_parser import decrypt_response
 from bestiary.models import Monster, Building
 from herders.models import MonsterInstance, RuneInstance, RuneCraftInstance, MonsterPiece, BuildingInstance
 from herders.profile_schema import HubUserLoginValidator, VisitFriendValidator

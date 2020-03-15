ee90d8cffd87e6069879e81742bdb148e11c5cf3
Peter Andersen <peter@porksmash.com>
2020-03-15 10:02:41 -0700

    Bump data logging game commands version

--- data_log/game_commands.py
+++ data_log/game_commands.py
@@ -102,7 +102,7 @@ active_log_commands = {
 accepted_api_params = {
     cmd: parser.accepted_commands for cmd, parser in active_log_commands.items()
 }
-accepted_api_params['__version'] = 7
+accepted_api_params['__version'] = 8
 
 
 # Utility functions

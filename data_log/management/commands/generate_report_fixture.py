9386236d41298ed8888a6774f40a15d44b7e53fe
QuatZo <48104274+QuatZo@users.noreply.github.com>
2021-04-06 19:43:02 +0200

    Create command for Data Log Report fixtures

---
+++ data_log/management/commands/generate_report_fixture.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from django.core import serializers
+
+from data_log import models
+import json
+
+
+class Command(BaseCommand):
+    help = 'Create Data Log Report fixtures'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Creating fixtures for Data Log Reports...')
+        JSONSerializer = serializers.get_serializer("json")
+        j = JSONSerializer()
+        data = []
+        models_to_serialize = [
+            models.LevelReport, 
+            models.SummonReport, 
+            models.MagicShopRefreshReport, 
+            models.MagicBoxCraftingReport, 
+            models.WishReport, 
+            models.RuneCraftingReport
+        ]
+
+        for model in models_to_serialize:
+            data += json.loads(j.serialize(model.objects.order_by('-generated_on')[:100]))
+        
+        data += json.loads(j.serialize(models.Report.objects.order_by('-generated_on')[:1000]))
+
+        with open("fixture_reports.json", "w+") as f:
+            json.dump(data, f)
+
+        self.stdout.write(self.style.SUCCESS('Done!'))

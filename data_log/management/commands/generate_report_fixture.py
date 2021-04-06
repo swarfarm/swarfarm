bad670aebbdeeb029a40762aae80eec1100268a2
QuatZo <48104274+QuatZo@users.noreply.github.com>
2021-04-06 19:57:00 +0200

    Fix data log fixture foreign keys

--- data_log/management/commands/generate_report_fixture.py
+++ data_log/management/commands/generate_report_fixture.py
@@ -22,13 +22,22 @@ class Command(BaseCommand):
             models.RuneCraftingReport
         ]
 
+        pks = []
         for model in models_to_serialize:
             self.stdout.write(self.style.WARNING(model.__name__))
-            data += json.loads(j.serialize(model.objects.order_by('-generated_on')[:100]))
+            serialized_data = json.loads(j.serialize(model.objects.order_by('-generated_on')[:100]))
+            pks += [d['pk'] for d in serialized_data]
+            data += serialized_data
         
         self.stdout.write(self.style.WARNING(models.Report.__name__))
-        data += json.loads(j.serialize(models.Report.objects.order_by('-generated_on')[:1000]))
+        reports = json.loads(j.serialize(models.Report.objects.order_by('-generated_on')[:100]))
 
+        self.stdout.write(self.style.HTTP_INFO("Finishing special reports..."))
+        reports += json.loads(j.serialize(models.Report.objects.filter(pk__in=pks)))
+
+        data = reports + data
+
+        self.stdout.write(self.style.HTTP_INFO('Saving fixtures to file'))
         with open("fixture_reports.json", "w+") as f:
             json.dump(data, f)
 

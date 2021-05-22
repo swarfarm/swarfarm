ab8acfa247d7f9f64e7d3f03f670c2df9f97feee
QuatZo <48104274+QuatZo@users.noreply.github.com>
2021-05-22 14:33:32 +0200

    Dark Mode - Charts

---
+++ static/main/js/highcharts_dark.js
@@ -0,0 +1,38 @@
+// set the theme
+Highcharts.setOptions({
+    title: {
+        style: {
+            color: '#ffffff',
+            font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
+        }
+    },
+    subtitle: {
+        style: {
+            color: '#ffffff',
+            font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
+        }
+    },
+    legend: {
+        itemStyle: {
+            font: '9pt Trebuchet MS, Verdana, sans-serif',
+            color: '#ffffff'
+
+        },
+        itemHoverStyle: {
+            color: '#bbbbbb'
+        },
+        itemHiddenStyle: {
+            color: 'gray'
+        }
+    },
+    credits: {
+        style: {
+            right: '10px'
+        }
+    },
+    labels: {
+        style: {
+            color: '#99b'
+        }
+    }
+});

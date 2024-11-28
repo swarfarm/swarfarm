ecdbca7257d4866eade4d05eabcc9826d126d530
Peter Andersen <pete.g.andersen@gmail.com>
2024-11-28 09:26:06 -0600

    Strip all non-API and postgres-specific features

--- bestiary/models/__init__.py
+++ bestiary/models/__init__.py
@@ -1,7 +1,5 @@
 from .base import Elements, Quality, Stats
 from .dungeons import Dungeon, Level, SecretDungeon, Wave, Enemy
-from .items import GameItem, ItemQuantity, Source, ESSENCE_MAP, LevelSkill
+from .items import GameItem, ItemQuantity, Source, ESSENCE_MAP
 from .monsters import Monster, AwakenCost, MonsterCraftCost, Fusion
-from .runes import RuneObjectBase, Rune, RuneCraft
 from .skills import Skill, SkillUpgrade, LeaderSkill, SkillEffect, SkillEffectDetail, ScalingStat, HomunculusSkill, HomunculusSkillCraftCost
-from .artifacts import Artifact, ArtifactCraft

06ea279406b84e18a25a4df1ded4d971fb7e9458
Peter Andersen <pete.g.andersen@gmail.com>
2024-12-02 21:43:26 -0600

    Remove celery-dependent code and settings

--- swarfarm/settings.py
+++ swarfarm/settings.py
@@ -20,7 +20,6 @@ env = environ.Env(
     EMAIL_HOST_USER=(str, ''),
     EMAIL_HOST_PASSWORD=(str, ''),
     EMAIL_FROM=(str, ''),
-    CELERY_BROKER=(str, 'amqp://'),
     GOOGLE_API_KEY=(str, ''),
     RECAPTCHA_PUBLIC_KEY=(str, ''),
     RECAPTCHA_PRIVATE_KEY=(str, ''),
@@ -222,12 +221,6 @@ CACHES = {
         'LOCATION': env('CACHE_LOCATION'),
     }
 }
-
-# Celery
-CELERY_BROKER_URL = env('CELERY_BROKER')
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_TRACK_STARTED = True
 
 # Session config
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
